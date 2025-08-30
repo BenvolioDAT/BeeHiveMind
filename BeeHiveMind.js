4be8c92e04dbd1c2eca4a75852ac875872d560a8
BenvolioDAT <92760165+BenvolioDAT@users.noreply.github.com>
2025-08-29 22:59:11 -0400

    Update BeeHiveMind.js

--- BeeHiveMind.js
+++ BeeHiveMind.js
@@ -79,10 +79,10 @@ const BeeHiveMind = {
             remoteharvest: 2,
             scout: 1,
             queen: 2,
-            CombatArcher: 2,
-            CombatMelee: 2,
+            CombatArcher: 4,
+            CombatMelee: 6,
             CombatMedic: 4,
-            Dismantler: 0,
+            Dismantler: 2,
             Trucker: 0,
 
         };
