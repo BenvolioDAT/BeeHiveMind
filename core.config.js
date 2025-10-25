c113d81f6c7285a287cbd18c389515905dcdaf9a
BenvolioDAT <92760165+BenvolioDAT@users.noreply.github.com>
2025-10-25 12:50:55 -0400

    Update core.config.js

--- core.config.js
+++ core.config.js
@@ -13,7 +13,7 @@ var settings = Object.freeze({
   }),
   pixels: Object.freeze({
     /** Toggle CPU bucket based pixel generation. */
-    enabled: true,
+    enabled: false,
     /** Minimum bucket value before attempting pixel generation. */
     bucketThreshold: 9900,
     /** Optional modulus so pixels are generated every N ticks. */
