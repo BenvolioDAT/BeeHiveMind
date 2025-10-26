c97579446cbd00f1081b49600e166d1022534253
BenvolioDAT <92760165+BenvolioDAT@users.noreply.github.com>
2025-10-26 17:58:34 -0400

    Remove deprecated planner proxies

--- Planner.Room.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// DEPRECATED: forwarded to Task.Builder.Planner.js
-var BuilderPlanner = require('Task.Builder.Planner');
-module.exports = {
-  plan: BuilderPlanner.plan,
-  ensureSites: BuilderPlanner.ensureSites
-};
