bf40053e47d04efc4b0315d72fc261114a094c17
BenvolioDAT <92760165+BenvolioDAT@users.noreply.github.com>
2025-11-08 17:51:33 -0500

    Update BeeHiveMind.js

--- BeeHiveMind.js
+++ BeeHiveMind.js
@@ -346,11 +346,11 @@ function computeRoomQuotas(C, room) {
     baseharvest:  2,
     courier:      1,
     queen:        1,
-    upgrader:     3,
+    upgrader:     5,
     builder:      getBuilderNeed(C, room),
     scout:        1,
     // Switch to determineLunaQuota(C, room) when you're ready:
-    luna:         1, // determineLunaQuota(C, room),
+    luna:         2, // determineLunaQuota(C, room),
     repair:       0,
     CombatArcher: 0,
     CombatMelee:  0,
