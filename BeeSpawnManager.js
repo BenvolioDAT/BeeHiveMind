90eb7c758b9817bee15bd57c76b37e00188760eb
BenvolioDAT <92760165+BenvolioDAT@users.noreply.github.com>
2026-05-12 12:56:46 -0400

    Update BeeSpawnManager.js

--- BeeSpawnManager.js
+++ BeeSpawnManager.js
@@ -327,7 +327,7 @@ function computeRoomQuotas(C, room) {
     Luna:         4,
     Repair:       0,
     Trucker:      0,
-    Claimer:      0,
+    Claimer:      2,
   };
   if (tickEvery(DBG_EVERY)) {
     dlog('🎯 [Quotas]', fmt(room), JSON.stringify(quotas));
